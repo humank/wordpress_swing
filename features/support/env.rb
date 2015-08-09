4004041699651e32b887ca13786109683ffae9cd
josephyao <dermott@126.com>
2015-08-09 21:32:20 +0800

    remove sleep in env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,7 +4,6 @@ path = File.expand_path(File.dirname(__FILE__))
 require File.expand_path(path + '/../../out/artifacts/WordPress_Swing_jar/WordPress-Swing.jar')
 
 Java::com.odde.atdd.demo::Main.main([].to_java(:string))
-sleep(1)
 
 at_exit do 
   java.lang.System.exit(0)
