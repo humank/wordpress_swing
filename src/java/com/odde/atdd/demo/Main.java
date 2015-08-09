e57e1d979fec0b931281b9e53ffedc5f654a6478
josephyao <dermott@126.com>
2015-08-09 21:20:00 +0800

    login with username and password but without backend validation

--- src/java/com/odde/atdd/demo/Main.java
+++ src/java/com/odde/atdd/demo/Main.java
@@ -7,12 +7,51 @@ public class Main {
 
     public static void main(String[] args) {
 
-        JFrame frame = new JFrame("Screenshot");
-
+        JFrame frame = new JFrame("WordPress-Swing");
         frame.setSize(new Dimension(640, 480));
 
-        frame.setVisible(true);
+        JPanel panel = new JPanel();
+        frame.setContentPane(panel);
 
+        panel.add(new JButton("login"));
+
+        addTextFieldWithLabel(panel, "user_name", "Please input user name");
+
+        addPasswordFieldWithLabel(panel, "password", "Please input password");
+
+        addTextFieldWithLabel(panel, "host_site", "Please input host site");
+
+        frame.setVisible(true);
+    }
+
+    private static void addPasswordFieldWithLabel(JPanel panel, String labelName, String defaultText) {
+        JLabel label = createAndAddLabel(panel, labelName);
+        JPasswordField password = createAndAddPassword(panel, defaultText);
+        label.setLabelFor(password);
+    }
+
+    private static void addTextFieldWithLabel(JPanel panel, String labelName, String defaultText) {
+        JLabel label = createAndAddLabel(panel, labelName);
+        JTextField text = createAndAddText(defaultText, label);
+        panel.add(text);
+    }
+
+    private static JPasswordField createAndAddPassword(JPanel panel, String defaultText) {
+        JPasswordField password = new JPasswordField(defaultText);
+        panel.add(password);
+        return password;
+    }
+
+    private static JLabel createAndAddLabel(JPanel panel, String labelName) {
+        JLabel label = new JLabel(labelName);
+        panel.add(label);
+        return label;
+    }
+
+    private static JTextField createAndAddText(String defaultText, JLabel label) {
+        JTextField text = new JTextField(defaultText);
+        label.setLabelFor(text);
+        return text;
     }
 
 }
