b33a7fabd0b20a50ffbf4ef1d96f19f8d28215dc
josephyao <dermott@126.com>
2015-08-14 21:48:54 +0800

    move dependencies and sut to a new file

---
+++ features/support/dependencies_and_sut.rb
@@ -0,0 +1,15 @@
+require 'java'
+
+lib_path = File.expand_path(File.dirname(__FILE__)) + '/../../lib/'
+require File.expand_path(lib_path + 'swinger/lib/swinger')
+require File.expand_path(lib_path + 'commons-cli-1.2.jar')
+require File.expand_path(lib_path + 'commons-collections-3.2.1.jar')
+require File.expand_path(lib_path + 'commons-configuration-1.8.jar')
+require File.expand_path(lib_path + 'commons-lang-2.6.jar')
+require File.expand_path(lib_path + 'commons-logging-1.1.1.jar')
+require File.expand_path(lib_path + 'json-20140107.jar')
+require File.expand_path(lib_path + 'jwordpress-0.6.2.jar')
+require File.expand_path(lib_path + 'xmlrpc-1.1.1.jar')
+
+require File.expand_path(lib_path + '/../out/artifacts/WordPress_Swing_jar/WordPress-Swing.jar')
+
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,17 +1,4 @@
-require 'java'
-
-require '/Users/josephyao/Documents/training_workspace/vagrant-android-develop/WordPress-Swing/lib/swinger/lib/swinger'
-require '/Users/josephyao/Documents/training_workspace/vagrant-android-develop/WordPress-Swing/lib/commons-cli-1.2.jar'
-require '/Users/josephyao/Documents/training_workspace/vagrant-android-develop/WordPress-Swing/lib/commons-collections-3.2.1.jar'
-require '/Users/josephyao/Documents/training_workspace/vagrant-android-develop/WordPress-Swing/lib/commons-configuration-1.8.jar'
-require '/Users/josephyao/Documents/training_workspace/vagrant-android-develop/WordPress-Swing/lib/commons-lang-2.6.jar'
-require '/Users/josephyao/Documents/training_workspace/vagrant-android-develop/WordPress-Swing/lib/commons-logging-1.1.1.jar'
-require '/Users/josephyao/Documents/training_workspace/vagrant-android-develop/WordPress-Swing/lib/json-20140107.jar'
-require '/Users/josephyao/Documents/training_workspace/vagrant-android-develop/WordPress-Swing/lib/jwordpress-0.6.2.jar'
-require '/Users/josephyao/Documents/training_workspace/vagrant-android-develop/WordPress-Swing/lib/xmlrpc-1.1.1.jar'
-
-path = File.expand_path(File.dirname(__FILE__))
-require File.expand_path(path + '/../../out/artifacts/WordPress_Swing_jar/WordPress-Swing.jar')
+require_relative 'dependencies_and_sut'
 
 Java::com.odde.atdd.demo::Main.main([].to_java(:string))
 
