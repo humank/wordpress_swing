903198e81a351aa5f4f24b79205ed73f9a469b05
josephyao <dermott@126.com>
2015-08-18 22:28:14 +0800

    refactor wordpress adaptor

---
+++ src/java/com/odde/atdd/demo/adaptor/RunnableWithXmlRpcExpections.java
@@ -0,0 +1,12 @@
+package com.odde.atdd.demo.adaptor;
+
+import net.bican.wordpress.exceptions.InsufficientRightsException;
+import net.bican.wordpress.exceptions.ObjectNotFoundException;
+import redstone.xmlrpc.XmlRpcFault;
+
+import java.net.MalformedURLException;
+
+public interface RunnableWithXmlRpcExpections {
+
+    void run() throws InsufficientRightsException, ObjectNotFoundException, XmlRpcFault, MalformedURLException;
+}
--- src/java/com/odde/atdd/demo/adaptor/WordPressAdaptor.java
+++ src/java/com/odde/atdd/demo/adaptor/WordPressAdaptor.java
@@ -1,7 +1,7 @@
 package com.odde.atdd.demo.adaptor;
 
-import com.odde.atdd.demo.model.Comment;
 import com.odde.atdd.demo.model.Credential;
+import net.bican.wordpress.Comment;
 import net.bican.wordpress.Post;
 import net.bican.wordpress.Wordpress;
 import net.bican.wordpress.exceptions.InsufficientRightsException;
@@ -26,28 +26,9 @@ public class WordPressAdaptor {
         }
     }
 
-    private Wordpress wordPressWithCredential(Credential credential) throws MalformedURLException {
-        return new Wordpress(credential.getUserName(), credential.getPassword(), xmlrpcUrl(credential));
-    }
-
-    private String xmlrpcUrl(Credential credential) {
-        return credential.getHostSite() + "/xmlrpc.php";
-    }
-
-    public void getAllPosts(Credential credential, final Consumer<com.odde.atdd.demo.model.Post> onEachPost) {
+    private void try_and_output_any_unexpected_exceptions(final RunnableWithXmlRpcExpections runnable) {
         try {
-            for (Post post : wordPressWithCredential(credential).getPosts())
-                onEachPost.accept(new com.odde.atdd.demo.model.Post(post.getPost_title(), post.getPost_id()));
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (XmlRpcFault xmlRpcFault) {
-            xmlRpcFault.printStackTrace();
-        }
-    }
-
-    public void comment(Credential credential, com.odde.atdd.demo.model.Post post, String comment) {
-        try {
-            wordPressWithCredential(credential).newComment(post.id, 0, comment, null, null, null);
+            runnable.run();
         } catch (InsufficientRightsException e) {
             e.printStackTrace();
         } catch (ObjectNotFoundException e) {
@@ -59,17 +40,31 @@ public class WordPressAdaptor {
         }
     }
 
-    public void getAllComments(Credential credential, com.odde.atdd.demo.model.Post post, Consumer<Comment> onEachComment) {
-        try {
-            for (net.bican.wordpress.Comment comment : wordPressWithCredential(credential).getComments("all", post.id, 0, 0)) {
-                onEachComment.accept(new Comment(comment.getContent()));
-            }
-        } catch (InsufficientRightsException e) {
-            e.printStackTrace();
-        } catch (XmlRpcFault xmlRpcFault) {
-            xmlRpcFault.printStackTrace();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+    private Wordpress wordPressWithCredential(Credential credential) throws MalformedURLException {
+        return new Wordpress(credential.getUserName(), credential.getPassword(), xmlrpcUrl(credential));
+    }
+
+    private String xmlrpcUrl(Credential credential) {
+        return credential.getHostSite() + "/xmlrpc.php";
+    }
+
+    public void getAllPosts(Credential credential, final Consumer<com.odde.atdd.demo.model.Post> onEachPost) {
+        try_and_output_any_unexpected_exceptions(() -> {
+            for (Post post : wordPressWithCredential(credential).getPosts())
+                onEachPost.accept(new com.odde.atdd.demo.model.Post(post.getPost_title(), post.getPost_id()));
+        });
+    }
+
+    public void comment(Credential credential, com.odde.atdd.demo.model.Post post, String comment) {
+        try_and_output_any_unexpected_exceptions(() -> {
+            wordPressWithCredential(credential).newComment(post.id, 0, comment, null, null, null);
+        });
+    }
+
+    public void getAllComments(Credential credential, com.odde.atdd.demo.model.Post post, Consumer<com.odde.atdd.demo.model.Comment> onEachComment) {
+        try_and_output_any_unexpected_exceptions(() -> {
+            for (Comment comment : wordPressWithCredential(credential).getComments("all", post.id, 0, 0))
+                onEachComment.accept(new com.odde.atdd.demo.model.Comment(comment.getContent()));
+        });
     }
 }
